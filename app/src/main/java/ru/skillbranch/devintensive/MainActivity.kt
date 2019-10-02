0d6f86928940fa02b4d8acbdc719f9589e534585
Artyom Mikhalev <thenostereal@gmail.com>
2019-10-02 21:31:16 +0300

    3rd homework done

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,68 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.EventLog
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
+import ru.skillbranch.devintensive.models.Bender
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bender: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
+
+        bender = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+        tv_text.text = bender.askQuestion()
+
+        Log.d("M_MainActivity", "onCreate $status, $question")
+
+        val (red, green, blue) = bender.status.color
+        iv_bender.setColorFilter(Color.rgb(red, green, blue), PorterDuff.Mode.MULTIPLY)
+
+        iv_send_btn.setOnClickListener {
+            onMessageSent()
+        }
+
+        et_message.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                onMessageSent()
+            }
+            return@setOnEditorActionListener true
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("STATUS", bender.status.name)
+        outState.putString("QUESTION", bender.question.name)
+        Log.d(
+            "M_MainActivity",
+            "onSaveInstanceState ${bender.status.name}, ${bender.question.name}"
+        )
+    }
+
+    private fun onMessageSent() {
+        val (phrase, color) = bender.listenAnswer(et_message.text.toString())
+        Log.d("M_MainActivity", "${et_message.text}, ${et_message.text.toString() in bender.question.answers}")
+        et_message.setText("")
+        val (r, g, b) = color
+        iv_bender.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+        tv_text.text = phrase
+
+        this.hideKeyboard()
     }
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,97 @@
+package ru.skillbranch.devintensive.models
+
+import android.util.Log
+import java.lang.IllegalStateException
+
+typealias Color = Triple<Int, Int, Int>
+
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
+
+    fun askQuestion(): String = when (question) {
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+    fun listenAnswer(answer: String): Pair<String, Color> {
+        Log.d("M_Bender", "answer: $answer")
+        return if (!question.isAnswerValid(answer)) {
+            when (question) {
+                Question.NAME ->
+                    "Имя должно начинаться с заглавной буквы\n${question.question}" to status.color
+                Question.PROFESSION ->
+                    "Профессия должна начинаться со строчной буквы\n${question.question}" to status.color
+                Question.MATERIAL ->
+                    "Материал не должен содержать цифр\n${question.question}" to status.color
+                Question.BDAY ->
+                    "Год моего рождения должен содержать только цифры\n${question.question}" to status.color
+                Question.SERIAL ->
+                    "Серийный номер содержит только цифры, и их 7\n${question.question}" to status.color
+                Question.IDLE -> throw IllegalStateException("it cannot happen")
+            }
+        } else if (question.answers.contains(answer.toLowerCase())) {
+            question = question.nextQuestion()
+            "Отлично - это правильный ответ!\n${question.question}" to status.color
+        } else {
+            status = status.nextStatus()
+            "Это неправильный ответ\n${question.question}" to status.color
+        }
+    }
+
+    enum class Status(val color: Color) {
+        NORMAL(Color(255, 255, 255)),
+        WARNING(Color(255, 120, 0)),
+        DANGER(Color(255, 60, 60)),
+        CRITICAL(Color(255, 0, 0));
+
+        fun nextStatus(): Status =
+            if (this.ordinal < values().lastIndex)
+                values()[this.ordinal + 1]
+            else
+                values()[0]
+    }
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("Бендер", "bender")) {
+            override fun isAnswerValid(answer: String): Boolean =
+                answer[0] == answer[0].toUpperCase()
+
+            override fun nextQuestion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override fun isAnswerValid(answer: String): Boolean =
+                answer[0] == answer[0].toLowerCase()
+
+            override fun nextQuestion(): Question = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+            override fun isAnswerValid(answer: String): Boolean =
+                !Regex("\\d").containsMatchIn(answer)
+
+            override fun nextQuestion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun isAnswerValid(answer: String): Boolean =
+                Regex("^\\d+$").matches(answer)
+
+            override fun nextQuestion(): Question = SERIAL
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override fun isAnswerValid(answer: String): Boolean =
+                Regex("^\\d{7}$").matches(answer)
+
+            override fun nextQuestion(): Question = IDLE
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun isAnswerValid(answer: String): Boolean = true
+
+            override fun nextQuestion(): Question = IDLE
+        };
+
+        abstract fun nextQuestion(): Question
+        abstract fun isAnswerValid(answer: String): Boolean
+    }
+}
